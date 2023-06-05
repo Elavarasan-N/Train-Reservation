47c2c06440fb6982b3a36da009654ad0313224cf
nelavarasan <elavarasan.rock.er.18@gmail.com>
2023-06-05 16:07:47 +0530

    new file added

---
+++ Main.java
@@ -0,0 +1,9 @@
+package trainReservation;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		reservation reserv = new reservation();
+		reserv.doMenu();
+		
+	}
+}
